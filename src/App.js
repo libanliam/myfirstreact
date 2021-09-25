99733160ec0a2a11bf74057287b8a65be5533249
libanliam <liam.liban.iics@ust.edu.ph>
2021-09-25 15:01:51 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          i love u kaye :>
+          hi sir :>
         </p>
         <a
           className="App-link"
